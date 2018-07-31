ef0aff65ca79608c6e6fc6dd16f735b1bc53dc12
Joan Subirats <subirats345@gmail.com>
2018-07-31 12:19:30 +0200

    ehem

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -17,7 +17,7 @@ export class AuthService {
     domain: 'subirats345.eu.auth0.com',
     responseType: 'token id_token',
     audience: 'https://subirats345.eu.auth0.com/userinfo',
-    redirectUri: 'http://sharingvotes.epizy.com/callback',
+    redirectUri: 'http://localhost:4200/callback',
     scope: 'openid profile'
   });
 
